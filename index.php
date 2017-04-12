bde9f3828c22f69c306b603d062348662f080557
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-04-12 08:22:23 -0600

    created css directory with style sheet

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 <html>
 	<head>
 		<meta charset="utf-8" >
+
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css" />
+
 		<title>HTML SANDBOX</title>
 	</head>
 	<body>
-		<h1>HAI!</h1>
+
 	</body>
 </html>
