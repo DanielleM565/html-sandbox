aadab1e870d6ce5061d25b3f99a0fdaba3e1987f
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-04-12 09:20:31 -0600

    added images and about directory

--- index.php
+++ index.php
@@ -6,9 +6,20 @@
 		<!-- Custom CSS -->
 		<link rel="stylesheet" href="css/style.css" type="text/css" />
 
+		<!-- Customer JavaScript -->
+		<script type="text/javascript" src="javascript/scripts.js"></script>
+
 		<title>HTML SANDBOX</title>
 	</head>
 	<body>
+		<main>
+			<h2>Commander Riker Says:</h2>
+		<p> In all trust, there is the possibility for betrayal. Yes, absolutely, I do indeed concur, wholeheartedly! What? We're not at all alike! The look in your eyes, I recognize it. You used to have it for me. The unexpected is our normal routine. Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? That might've been one of the shortest assignments in the history of Starfleet. Then maybe you should consider this: if anything happens to them, Starfleet is going to want a full investigation. Now, how the hell do we defeat an enemy that knows us better than we know ourselves?
+		</p>
+			<img src="img/prospurr.jpg" alt="live long and prospurr"/>
+			<p>This is a link to my favorite website: <a href="http://www.tacoipsum.com/" target="_blank">taco</a></p>
+			<p>This is a link to an internal page: <a href="about/index.php">About Page</a></p>
 
+		</main>
 	</body>
 </html>
---
+++ javascript/scripts.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Owner on 4/12/2017.
+ */
